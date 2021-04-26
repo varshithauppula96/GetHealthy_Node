f4a7f17ecbd561ec50b33162d289cb79cf881f42
rishabhdutta01 <rishabh.dutta01@gmail.com>
2021-04-25 22:27:05 -0400

    adding more feedback changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const foodTracker = require("./routes/api/food-tracker");
+const feedback = require("./routes/api/feedback");
 const app = express();
 
 const cors = require("cors")
@@ -48,6 +49,7 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/foodEntries", foodTracker)
+app.use("/api/feedback", feedback)
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
