fd459f988c6688630013e01346205a8c1c23cc9a
Varshitha Uppula <varshithauppula@gmail.com>
2021-04-28 10:19:04 -0400

    json package change

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://GetHealthyUser:normalUser@cluster0.nrmed.mongodb.net/GET_HEALTHY?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://GetHealthyUser:varshitha098765@cluster0.nrmed.mongodb.net/GET_HEALTHY?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
