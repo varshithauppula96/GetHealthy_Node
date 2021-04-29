cd58410bdf0c362f6f23811b49b34d70f4474ac0
rishabhdutta01 <rishabh.dutta01@gmail.com>
2021-04-29 02:55:38 -0400

    pushing all profile page changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -96,7 +96,7 @@ router.post("/register", (req, res) => {
                 trainerId:req.body.trainerId,
                 about:req.body.about
             });
-// Hash password before saving in database
+            // Hash password before saving in database
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
@@ -117,19 +117,19 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
     // Form validation
     const { errors, isValid } = validateLoginInput(req.body);
-// Check validation
+    // Check validation
     if (!isValid) {
         return res.status(400).json(errors);
     }
     const email = req.body.email;
     const password = req.body.password;
-// Find user by email
+    // Find user by email
     User.findOne({ email }).then(user => {
         // Check if user exists
         if (!user) {
             return res.status(404).json({ emailnotfound: "Email not found" });
         }
-// Check password
+        // Check password
         bcrypt.compare(password, user.password).then(isMatch => {
             if (isMatch) {
                 // User matched
@@ -147,7 +147,7 @@ router.post("/login", (req, res) => {
                     trainerId:user.trainerId,
                     about:user.about
                 };
-// Sign token
+                // Sign token
                 jwt.sign(
                     payload,
                     keys.secretOrKey,
@@ -202,40 +202,6 @@ router.get("/:trainerId/trainees", async (req, res) => {
     }
 })
 
-// @route POST api/users/register
-// @desc Register user
-// @access Public
-// router.put("/profile", (req, res) => {
-//     // Form validation
-//     //const { errors, isValid } = validateRegisterInput(req.body);
-// // Check validation
-// //     if (!isValid) {
-// //         return res.status(400).json(errors);
-// //     }
-//
-//     User.findOne({ email: req.body.email }).then(user => {
-//         if (user) {
-//                 user.name= req.body.name
-//                 user.email= req.body.email
-//                 //user.password= req.body.password
-//                 user.userType= req.body.userType
-//
-// // Hash password before saving in database
-//             bcrypt.genSalt(10, (err, salt) => {
-//                 bcrypt.hash(user.password, salt, (err, hash) => {
-//                     if (err) throw err;
-//                     user.password = hash;
-//                     user.save()
-//                         .then(user => res.json(user))
-//                         .catch(err => console.log(err));
-//                 });
-//             });
-//         } else {
-//             return res.status(400).json({ email: "Email does not exists!!!" });
-//         }
-//     });
-// });
-
 router.put("/profile/:userId", async (req, res) => {
     try {
         const userId = req.params['userId'];
@@ -247,71 +213,45 @@ router.put("/profile/:userId", async (req, res) => {
                 'heightInCms':req.body.heightInCms,
                 'about':req.body.about
             }})
-        console.log("updation===" + user)
-        res.send(user)
+
+        const email = req.body.email;
+        User.findOne({ email }).then(user => {
+            // User matched
+            // Create JWT Payload
+            const payload = {
+                _id: user._id,
+                password: user.password,
+                name: user.name,
+                email:user.email,
+                gender:user.gender,
+                dateOfBirth:user.dateOfBirth,
+                weightInKgs:user.weightInKgs,
+                heightInCms:user.heightInCms,
+                userType:user.userType,
+                trainerId:user.trainerId,
+                about:user.about
+            };
+            // Sign token
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                {
+                    expiresIn: 31556926 // 1 year in seconds
+                },
+                (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    });
+                }
+            );
+
+        });
     }
     catch (error) {
         console.error(error.message)
         res.status(500).send("Server Error")
     }
-})
-
-// router.put("/profile/:userId", async (req, res) => {
-//     try {
-//         const userId = req.params['userId'];
-//         console.log("useriD is = ===== " + userId)
-//         User.updateOne({_id: userId}, {
-//             $set: {
-//                 name: req.body.name,
-//                 gender: req.body.gender,
-//                 weightInKgs: req.body.weightInKgs,
-//                 heightInCms: req.body.heightInCms,
-//                 about: req.body.about,
-//             }
-//         })
-//
-//         const user = await User.findById(userId)
-//         console.log("Updated user====" + user)
-//         res.send(user)
-//
-//
-//
-//         //     .then(user => {
-//         //     // Create JWT Payload
-//         //     const payload = {
-//         //         _id: user._id,
-//         //         password: user.password,
-//         //         name: user.name,
-//         //         email: user.email,
-//         //         gender: user.gender,
-//         //         dateOfBirth: user.dateOfBirth,
-//         //         weightInKgs: user.weightInKgs,
-//         //         heightInCms: user.heightInCms,
-//         //         userType: user.userType,
-//         //         trainerId: user.trainerId,
-//         //         about: user.about
-//         //     };
-//         //     console.log("user created" + JSON.stringify(user))
-//         //     // Sign token
-//         //     jwt.sign(
-//         //         payload,
-//         //         keys.secretOrKey,
-//         //         {
-//         //             expiresIn: 31556926 // 1 year in seconds
-//         //         },
-//         //         (err, token) => {
-//         //             res.json({
-//         //                 success: true,
-//         //                 token: "Bearer " + token
-//         //             });
-//         //         }
-//         //     );
-//         // });
-//     }
-//     catch (error) {
-//         console.error(error.message)
-//         res.status(500).send("Server Error")
-//     }
-// })
+});
 
 module.exports = router;
